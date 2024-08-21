bf5f1e066a6d90bdc2b975c086783d9037f80a3e
Ruslan <ruslan.sidorenko@madnesspartners.com>
2024-08-21 15:52:43 +0500

    new display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
+  before_action :set_language
+
   def index
+    request.variant = browser.device.mobile? ? :mobile : :desktop
+  end
+
+  def set_language
+    if params[:lang].present?
+      cookies.permanent[:lang] = params[:lang]
+    end
+
+    lang = cookies[:lang]&.to_sym
+    if I18n.available_locales.include?(lang)
+      I18n.locale = lang
+    end
   end
 end
