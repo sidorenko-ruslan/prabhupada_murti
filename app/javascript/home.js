3eeabbc187208aca71a151a6b183e1cdba1e2ab1
mikhail-seleshe <mikhail.seleshe@gmail.com>
2024-11-24 18:06:34 +0100

    update dot with comma for $ price

--- app/javascript/home.js
+++ app/javascript/home.js
@@ -2,7 +2,7 @@
 import Splide from '@splidejs/splide';
 
 function numberWithSpaces(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ").replace('.', ',');
 }
 
 let inputBuyMurtiCount = document.getElementById("buy-murti-count");
