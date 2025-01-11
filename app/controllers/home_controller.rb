75ff7bcbe7763cdadbfd9f98b9d499fe47c530bf
mikhail-seleshe <mikhail.seleshe@gmail.com>
2025-01-11 12:27:33 +0100

    update home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,6 +22,10 @@ class HomeController < ApplicationController
     @order.murti_count = params[:murti_count]
   end
 
+  def add_disciple
+    
+  end
+
   def make_purchase
     @order = find_previous_unpayed_order
     @order.address = order_params[:address] if @order
