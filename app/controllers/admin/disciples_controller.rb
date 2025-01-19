56b70a403d2be1e46a2e8e3ad86aaced64491c12
mikhail-seleshe <mikhail.seleshe@gmail.com>
2025-01-19 03:42:29 +0100

    update disciples_controller

--- app/controllers/admin/disciples_controller.rb
+++ app/controllers/admin/disciples_controller.rb
@@ -1,9 +1,12 @@
 class Admin::DisciplesController < ApplicationController
   layout "application_admin"
 
+  DEFAULT_ORDER = {created_at: :asc}
+  
   before_action :authenticate_user!
 
   def index
-    @disciples = DiscipleInfo.all.order(:spritual_name)
+    recors_order = params[:filter] ? { params[:filter] => params[:order] } : DEFAULT_ORDER
+    @disciples = DiscipleInfo.order(:recors_order)
   end
 end
