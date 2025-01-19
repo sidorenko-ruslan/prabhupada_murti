255b8e30a33f292f068f1fc6d98f25e7db618d8d
mikhail-seleshe <mikhail.seleshe@gmail.com>
2025-01-19 04:03:13 +0100

    update disciples_helper

--- app/helpers/admin/disciples_helper.rb
+++ app/helpers/admin/disciples_helper.rb
@@ -1,2 +1,29 @@
 module Admin::DisciplesHelper
+  def disciple_filter_columns
+    {
+      "id" => "Номер анкеты",
+      "spritual_name" => "Духовное имя",
+      "initiation" => "Инициация",
+      "fullname" => "Ф. И. О.",
+      "address" => "Адрес",
+      "phone" => "Телефон",
+      "email" => "Имейл",
+      "imdisciple" => "Я ученик?",
+      "imgivingcontact" => "Я даю контакт на ученика?",
+      "created_at" => "Дата создания",
+      "updated_at" => "Дата редактирования"
+    }
+  end
+
+  def disciple_direction(name, filter, disciple)
+    return "asc" unless name == filter
+
+    disciple == "asc" ? "desc" : "asc"
+  end
+
+  def disciple_arror(name, filter, disciple)
+    return "" unless name == filter
+
+    disciple == "asc" ? "bi-arrow-up" : "bi-arrow-down"
+  end
 end
