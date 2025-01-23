e27ec5597f6b17cf09c6abe5aaa0b22369f60214
mikhail-seleshe <mikhail.seleshe@gmail.com>
2025-01-23 02:58:19 +0100

    update create initiations migration

--- db/migrate/20250123022934_create_initiations.rb
+++ db/migrate/20250123022934_create_initiations.rb
@@ -6,15 +6,15 @@ class CreateInitiations < ActiveRecord::Migration[7.1]
       t.string :status
     end
 
-    Initiations.create(name: "Abhaya Devi Dasi", place: "Portland February 1975", status: "")
-    Initiations.create(name: "Abhayasraya Dasa", place: "Delhi August 1976", status: "")
-    Initiations.create(name: "Abhilasa Dasa", place: "Vrndavana April 1976", status: "")
-    Initiations.create(name: "Abhimanyumsi Dasa", place: "London August 1972", status: "")
-    Initiations.create(name: "Abhimanyusuta Dasa", place: "Lima September 1977", status: "")
-    Initiations.create(name: "Abhinanda Dasa", place: "Seattle August 1973", status: "")
-    Initiations.create(name: "Abhinanda Dasa", place: "San Diego 1973", status: "")
-    Initiations.create(name: "Abhinanda Dasa", place: "London September 1973", status: "")
-    Initiations.create(name: "Abhinava Dasa", place: "Laguna January 1972", status: "")
-    Initiations.create(name: "Abhipraya Dasa", place: "Los Angeles June 1974", status: "")
+    Initiation.create(name: "Abhaya Devi Dasi", place: "Portland February 1975", status: "")
+    Initiation.create(name: "Abhayasraya Dasa", place: "Delhi August 1976", status: "")
+    Initiation.create(name: "Abhilasa Dasa", place: "Vrndavana April 1976", status: "")
+    Initiation.create(name: "Abhimanyumsi Dasa", place: "London August 1972", status: "")
+    Initiation.create(name: "Abhimanyusuta Dasa", place: "Lima September 1977", status: "")
+    Initiation.create(name: "Abhinanda Dasa", place: "Seattle August 1973", status: "")
+    Initiation.create(name: "Abhinanda Dasa", place: "San Diego 1973", status: "")
+    Initiation.create(name: "Abhinanda Dasa", place: "London September 1973", status: "")
+    Initiation.create(name: "Abhinava Dasa", place: "Laguna January 1972", status: "")
+    Initiation.create(name: "Abhipraya Dasa", place: "Los Angeles June 1974", status: "")
   end
 end
